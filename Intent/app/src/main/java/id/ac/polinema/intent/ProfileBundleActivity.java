472318fef7c26dba51c8ea3f06c84d9c9daa21e0
LyneD <krisna.widi.kw@gmail.com>
2019-09-20 16:56:01 +0700

    Explicit, Implicit, dan Bundle, Parcelable(Belum)

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -3,19 +3,38 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ProfileBundleActivity extends AppCompatActivity {
+
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_bundle);
 
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
+
         // TODO: bind here
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             // TODO: display value here
+            //String username = input_username.getText().toString();
+            //String name = input_name.getText().toString();
+            //String age = input_age.getText().toString();
+
+            usernameText.setText(extras.getString("username"));
+            nameText.setText(extras.getString("name"));
+            ageText.setText(String.valueOf(extras.getInt(BundleActivity.AGE_KEY)));
+
+            //usernameText.setText(getIntent().getStringExtra());
         }
     }
 }
